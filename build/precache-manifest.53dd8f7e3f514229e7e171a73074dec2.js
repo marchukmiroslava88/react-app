d5aedd16ff02937a2a6cb87f5942e6dd8c9bf98c
Miroslava <m.marchuk@kitsoft.kiev.ua>
2020-09-24 12:08:01 +0300

    1.3

---
+++ build/precache-manifest.53dd8f7e3f514229e7e171a73074dec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7dee7339b0af40f9e4dea09d9a29a5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7e194c1a8153020efcd",
+    "url": "static/css/main.843b31e5.chunk.css"
+  },
+  {
+    "revision": "921f23917865e85917bd",
+    "url": "static/js/2.c53fec5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "static/js/2.c53fec5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7e194c1a8153020efcd",
+    "url": "static/js/main.35e7f1f5.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "static/js/runtime-main.92ee656d.js"
+  }
+]);
